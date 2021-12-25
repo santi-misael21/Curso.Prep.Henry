c2dbebc28a26f5c4ab02097f01b5d5116e765c6e
santiago alamos <santiagoalamos2000@gmail.com>
2021-12-25 15:14:22 -0300

    nadaporelestil

--- Nueva carpeta/Nueva carpeta/Untitled-1.test.js
+++ Nueva carpeta/Nueva carpeta/Untitled-1.test.js
@@ -1,9 +1,9 @@
 const {
     crearUsuario,
-} = require('.Untitled-2');
+} = require('../Untitled-2');
 
 describe('crearUsuario()', function() {
-	it("string obligatorio indeterminante", function() {
+	t("string obligatorio indeterminante", function() {
 		const Usuario = crearUsuario();
 		const user = new Usuario({usuario: 'jssamuel', nombre: 'Samuel', email: 'samuel@email.com', password: 'LoveJS' });
 		expect(user.usuario).toBe('jssamuel');
