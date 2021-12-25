1fd50733fbc1148f7f12ba183c6b68fd2a61a578
santiago alamos <santiagoalamos2000@gmail.com>
2021-12-25 00:56:47 -0300

    nadaporelestil

--- Nueva carpeta/Untitled-2.js
+++ Nueva carpeta/Untitled-2.js
@@ -1,22 +1,20 @@
 function crearUsuario() {
-    class Usuario {
-    constructor(opciones) {
+    function Usuario(opciones) {
       this.usuario = opciones.usuario;
       this.nombre = opciones.nombre;
       this.email = opciones.email;
       this.password = opciones.password;
     }
-    saludar() {
-      return 'Hola, mi nombre es ' + this.nombre;
-    }
-  }
   
+    Usuario.prototype.saludar = function() {
+      return 'Hola, mi nombre es ' + this.nombre;
+    };
   
     return Usuario;
   
   }
 
-  export default {
+  module.exports = {
     crearUsuario,
   };
   
