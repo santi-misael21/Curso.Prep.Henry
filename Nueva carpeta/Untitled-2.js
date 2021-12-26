218d0b0c5d2cf3aa1ecba2a1ec0dd4b3458d9b23
santiago alamos <santiagoalamos2000@gmail.com>
2021-12-25 23:16:30 -0300

    nadaporelestil

--- Nueva carpeta/Untitled-2.js
+++ Nueva carpeta/Untitled-2.js
@@ -1,7 +1,8 @@
 function operacionMatematica(n1, n2, cb) {
-  function cb (n1,n2) {
-    return cb (n1, n2)
-  }
+  //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
+  //Devolver el callback pasándole como argumentos los números recibidos.
+  //Tu código:
+  return cb (n1 + n2)
 }
   
 module.exports = {
